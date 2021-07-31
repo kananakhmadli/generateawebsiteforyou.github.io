573c40ba2c5f6a3e99db56a42c2343be8a7be7d9
kanan <kanan.akhmadli99@gmail.com>
2021-08-01 01:39:52 +0400

    demo

--- calculator.js
+++ calculator.js
@@ -7,13 +7,10 @@ let genderStatus = true;
 //false  female
 genders.forEach((element) => {
     element.addEventListener("change", function () {
-        // console.log(element.value)
         if (element.value == "female") {
-            console.log("hell")
             genderStatus = false
         }
         else {
-            console.log("yes")
             genderStatus = true;
         }
     })
@@ -23,16 +20,13 @@ let ageInput = 0;
 
 let age = document.querySelector("#age");
 age.addEventListener("input", function () {
-    // console.log(this.value)
     ageInput = this.value
-    console.log(typeof ageInput)
 })
 
 let heightInput = 0;
 
 let height = document.querySelector("#height");
 height.addEventListener("input", function () {
-    console.log(this.value)
     heightInput = this.value;
 })
 
@@ -40,7 +34,6 @@ let weightInput = 0;
 
 let weight = document.querySelector("#weight");
 weight.addEventListener("input", function () {
-    console.log(this.value);
     weightInput = this.value;
 })
 
@@ -49,12 +42,12 @@ let activityRate = document.querySelectorAll('[name="amr"]');
 let activityRateInput = "lightly";
 activityRate.forEach((element) => {
     element.addEventListener("change", function () {
-        console.log(element.id)
         activityRateInput = element.id;
     })
 })
 
 function blinker() {
+
     if (genderStatus) {
         bmr = 66.47 + 13.75 * Number(weightInput) + 5.003 * Number(heightInput) - 6.755 * Number(ageInput);
     }
@@ -66,7 +59,6 @@ function blinker() {
         amr = bmr * 1.2;
     }
     else if (activityRateInput == "lightly") {
-        console.log("Here")
         amr = bmr * 1.375;
     }
     else if (activityRateInput == "moderately") {
@@ -78,17 +70,12 @@ function blinker() {
     else {
         amr = bmr * 1.9;
     }
-    console.log(`Gender is ${genderStatus},age is ${ageInput}, height is ${heightInput},weight is ${weightInput},
-    activityRateInput is ${activityRateInput},amr is ${amr},Result is ${amr + amr / 15}
-    `)
+    // console.log(`Gender is ${genderStatus},age is ${ageInput}, height is ${heightInput},weight is ${weightInput},
+    // activityRateInput is ${activityRateInput},amr is ${amr},Result is ${amr + amr / 15}
+    // `)
+
     let result = document.querySelector("#result");
     result.value=Math.round(amr + amr / 40);
-    result.value+="%";
-
-
 
 }
-// console.log(result)
-// result.addEventListener("")
-
 setInterval(blinker, 500);
